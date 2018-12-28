2ba8b8aeeceb2fc75564290516669d9a334f9f62
Ross <rostyslav.hutii@orilinc.com>
2018-12-28 15:05:33 +0200

    refactored standard App and root components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from 'pages/Home';
+import YourPage from 'pages/YourPage';
+import NotFound from 'pages/NotFound';
+
+import { root, yourPageRoute } from 'routes/internal';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Fragment>
+          <header>
+            {
+              // Define Header component 
+            }
+          </header>
+          <main id="main">
+            <Switch>
+              <Route path={root} exact component={Home} />
+              <Route path={yourPageRoute} exact component={YourPage} />
+              <Route component={NotFound} />
+            </Switch>
+          </main>
+          <footer>
+              {
+                // Define Footer component 
+              }
+          </footer>
+        </Fragment>
       </div>
     );
   }
