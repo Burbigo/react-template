2bf4ab58fb036c26b53baa08b2e199459b8a8027
Ross <rostyslav.hutii@orilinc.com>
2018-12-28 15:04:53 +0200

    added routes folder

---
+++ src/routes/external.js
@@ -0,0 +1,3 @@
+// Define all routes which would be used as navigation to external resources (sites)
+
+export const google = 'google.com';
---
+++ src/routes/internal.js
@@ -0,0 +1,5 @@
+// Define all routes which would be used as navigation to internal resources (pages)
+
+export const root = '/';
+export const yourPageRoute = '/your-page';
+export const notFound = '/not-found';
